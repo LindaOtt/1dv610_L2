85b086c83b048d16f4d80dfb0abd3882f227f2c6
Linda Ott Olander <lo222hd@student.lnu.se>
2017-09-16 20:15:42 +0200

    Test case 1.1.

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -3,10 +3,15 @@
 class DateTimeView {
 
 
-	public function show() {
-
-		$timeString = 'TODO, Write servertime here...';
+	public function showDateAndTime() {
+		date_default_timezone_set('Europe/Stockholm');
+		//Todays date is shown as: [Day of week], the [day of month numeric]th of
+		//[Month as text] [year 4 digits]. The time is [Hour]:[minutes]:[Seconds].
+		//Example: "Monday, the 8th of July 2015, The time is 10:59:21".
+		$timeString = date('l, \t\h\e j\t\h \o\f F Y. \T\h\e \t\i\m\e \i\s H:i:s', time());
 
 		return '<p>' . $timeString . '</p>';
 	}
+
+
 }
