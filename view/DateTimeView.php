dac387e7506b89c8af810c005a5b44c71e30e367
Linda Ott Olander <lo222hd@student.lnu.se>
2017-09-17 15:34:28 +0200

    Test case 1.1

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -8,7 +8,7 @@ class DateTimeView {
 		//Todays date is shown as: [Day of week], the [day of month numeric]th of
 		//[Month as text] [year 4 digits]. The time is [Hour]:[minutes]:[Seconds].
 		//Example: "Monday, the 8th of July 2015, The time is 10:59:21".
-		$timeString = date('l, \t\h\e j\t\h \o\f F Y. \T\h\e \t\i\m\e \i\s H:i:s', time());
+		$timeString = date('l, \t\h\e j\t\h \o\f F Y, \T\h\e \t\i\m\e \i\s H:i:s', time());
 
 		return '<p>' . $timeString . '</p>';
 	}
