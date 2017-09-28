c151936f531f438f89dc401734d67a3dd308a93b
Linda Ott Olander <lo222hd@student.lnu.se>
2017-09-28 11:13:25 +0200

    Test case 3.1

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -19,20 +19,20 @@ class LoginController {
     $isLoggedIn = false;
 
     if ($this->user->getIsLoggedInWithSession() && $this->user->getHasLoggedOut()) {
-      echo "1. getIsLoggedInWithSession and getHasLoggedOut";
+      //echo "1. getIsLoggedInWithSession and getHasLoggedOut";
       $this->user->setIsLoggedInWithSession(false);
       $this->user->terminateLoginSession();
     }
     else if (!$this->user->getIsLoggedInWithSession() && $this->user->getHasLoggedOut()) {
-      echo "2. NOT getIsLoggedInWithSession and getHasLoggedOut";
+      //echo "2. NOT getIsLoggedInWithSession and getHasLoggedOut";
       $this->user->setHasLoggedOutWithoutSession(true);
     }
     else if ($this->user->getIsLoggedInWithSession() && !$this->user->getHasLoggedOut()) {
-      echo "3. getIsLoggedInWithSession and NOT getHasLoggedOut";
+      //echo "3. getIsLoggedInWithSession and NOT getHasLoggedOut";
       $isLoggedIn = true;
     }
     else if ($this->user->getHasJustTriedToLogIn() && $this->user->getIsLoggedInWithForm()) {
-      echo "4. getHasJustTriedToLogIn and getIsLoggedInWithForm";
+      //echo "4. getHasJustTriedToLogIn and getIsLoggedInWithForm";
       $isLoggedIn = true;
       $keepUserLogin = false;
       //Check if user has chosen to be kept logged in
@@ -42,24 +42,10 @@ class LoginController {
       $this->user->createLoginSession($keepUserLogin);
     }
     else if ($this->user->getIsLoggedInWithSession()) {
-      echo "5. getIsLoggedInWithSession";
+      //echo "5. getIsLoggedInWithSession";
       $isLoggedIn = true;
     }
     $this->layoutView->render($isLoggedIn, $this->user, $this->loginView, $this->dateTimeView);
-
-    /*
-    if ($this->user->getHasLoggedOut()) {
-      //Check if user is logged in
-      if ($this->user->getIsLoggedInWithSession()) {
-        $this->user->setIsLoggedInWithSession(false);
-        $this->user->terminateLoginSession();
-      }
-      else {
-
-      }
-      $this->layoutView->render(false, $this->user, $this->loginView, $this->dateTimeView);
-    }
-    */
   }
 
 }
