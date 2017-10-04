574c4c14a8180cb509e9c553019499b973a37b3b
Linda Ott Olander <lo222hd@student.lnu.se>
2017-10-04 15:26:42 +0200

    Refactoring

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -26,7 +26,55 @@ class LoginController {
       $wantsToRegisterUser = true;
     }
 
+    //The user is logged in with a session
+    if ($this->loginModel->getIsLoggedInWithSession() {
+      //The user has just clicked the "logout" button
+      if ($this->loginModel->getHasLoggedOut()) {
+        $this->loginModel->setIsLoggedInWithSession(false);
+        $this->loginModel->terminateLoginSession();
+      }
+      //The user has not logged out
+      else {
+        $isLoggedIn = true;
+      }
+    }
+    //The user is not logged in with a session
+    else {
+      //The user has just clicked the "logout" button
+      if ($this->loginModel->getHasLoggedOut()) {
+          $this->loginModel->setHasLoggedOutWithoutSession(true);
+      }
+      //The user has just tried to log in with the log in form
+      //The login was successful, username and password are correct
+      else if ($this->loginModel->getHasJustTriedToLogIn() && $this->loginModel->getIsLoggedInWithForm()) {
+        $isLoggedIn = true;
+        $keepUserLogin = false;
+        $this->loginModel->createLoginSession();
+        //The user has clicked "keep me logged in" in the form
+        if ($this->loginModel->getKeepUserLoggedIn() == true) {
+          $this->loginModel->createLoginCookies(time()+180, true);
+        }
+      }
+      //The user has not clicked the "logout" button
+      //and has not tried to log in with the log in form
+      else {
+        //Check if the user is logged in with cookies
+        if ($this->loginModel->getIsLoggedInWithCookies()) {
+          //Check if the cookies are ok ()
+          if ($this->loginModel->getIsCookieContentOK()) {
+            $isLoggedIn = true;
+          }
+          else {
+            //Remove cookies
+            $this->loginModel->createLoginCookies(time()-1000, false);
+            $isLoggedIn = false;
+          }
+        }
+      }
+    }
+
     //The user is logged in with a session, but has just clicked the "logout" button
+    /*
     if ($this->loginModel->getIsLoggedInWithSession() && $this->loginModel->getHasLoggedOut()) {
       $this->loginModel->setIsLoggedInWithSession(false);
       $this->loginModel->terminateLoginSession();
@@ -71,6 +119,7 @@ class LoginController {
         }
       }
     }
+    */
     $this->layoutView->render($isLoggedIn, $wantsToRegisterUser, $this->loginModel, $this->loginView, $this->dateTimeView);
   }
 }
