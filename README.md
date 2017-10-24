fb2e09bad1b8977e01ce73775c685436c9a186c1
Linda Ott Olander <lo222hd@student.lnu.se>
2017-10-24 23:07:31 +0200

    Test case 5.2

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Login_1DV608
 Interface repository for 1DV608 assignment 2 and 4
+
+#Test case 5.1, Show time zone dropdown
+Normal navigation to page, page is shown.
+
+**Input:**
+- Test case 1.1.
+
+**Output:**
+- The text "Not logged in", is shown
+- A form for login is shown
+- Todays date and time is shown in correct format.
+- Underneath todays date and time, there is a dropdown list with five different time zones. The default timezone "Europe/Stockholm" is picked.
+Next to the dropdown list is some text says "Pick timezone".
+Next to the text is a submit button that says "Change".
+
+#Test case 5.2, Change time zone
+**Input:**
+- Test case 5.2.
+- Pick a timezone from the list
+- Click the "Change" button
+
+**Output:**
+- The time is updated and reflects the picked timezone.
+- The dropdown list shows the picked timezone.
--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -6,8 +6,15 @@ class DateTimeView {
 	private static $timeselect = 'timeselect';
 	private static $timepick = 'DateTimeView::TimePick';
 	private $selectedTimeZone = 'stockholm';
-
 	private static $changeTimeZone = "changetimezone";
+
+	private $timeZones = array(
+    0 => array(0 => 'stockholm', 1 => 'Europe/Stockholm'),
+    1 => array(0 => 'london', 1 => 'Europe/London'),
+		2 => array(0 => 'tallinn', 1 => 'Europe/Tallinn'),
+		3 => array(0 => 'moscow', 1 => 'Europe/Moscow'),
+		4 => array(0 => 'paris', 1 => 'Europe/Paris'),
+	);
 
 	public function __construct() {
 		if ($this->wantsToChangeTimeZone()) {
@@ -15,6 +22,7 @@ class DateTimeView {
 		}
 	}
 
+/*
 	public function showDateAndTime() {
 		switch ($this->selectedTimeZone) {
 			case "stockholm":
@@ -46,17 +54,55 @@ class DateTimeView {
 
 		return '<p>' . $timeString . '</p>';
 	}
+	*/
+
+	public function showDateAndTime() {
+		switch ($this->selectedTimeZone) {
+			case $this->timeZones[0][0]:
+				error_log("timezone stockholm", 3, "errors.log");
+				date_default_timezone_set($this->timeZones[0][1]);
+				break;
+			case $this->timeZones[1][0]:
+				error_log("timezone london", 3, "errors.log");
+			 	date_default_timezone_set($this->timeZones[1][1]);
+				break;
+			case $this->timeZones[2][0]:
+				error_log("timezone tallinn", 3, "errors.log");
+				date_default_timezone_set($this->timeZones[2][1]);
+				break;
+			case $this->timeZones[3][0]:
+				error_log("timezone moscow", 3, "errors.log");
+				date_default_timezone_set($this->timeZones[3][1]);
+				break;
+			case $this->timeZones[4][0]:
+				error_log("timezone paris", 3, "errors.log");
+				date_default_timezone_set($this->timeZones[4][1]);
+				break;
+			default:
+				error_log("timezone default", 3, "errors.log");
+				date_default_timezone_set($this->timeZones[0][1]);
+		}
+
+		$timeString = date('l, \t\h\e jS \o\f F Y, \T\h\e \t\i\m\e \i\s H:i:s', time());
+
+		return '<p>' . $timeString . '</p>';
+	}
 
 	public function showChangeTimeZoneForm() {
 		$ret = '
 			<form action="?'. self::$changeTimeZone .'" method="post" enctype="multipart/form-data">
 					<label for="' . self::$timezone . '">Pick timezone :</label>
-					<select name="' . self::$timeselect . '">
-  					<option value="stockholm">Europe/Stockholm</option>
-  					<option value="london">Europe/London</option>
-  					<option value="tallinn">Europe/Tallinn</option>
-						<option value="moscow">Europe/Moscow</option>
-						<option value="paris">Europe/Paris</option>
+					<select name="' . self::$timeselect . '">';
+
+						foreach ($this->timeZones as list($city, $location)) {
+								$ret .= '<option value="'.$city.'"';
+								if ($city == $this->selectedTimeZone) {
+									$ret .= ' selected';
+								}
+								$ret .= '>'.$location.'</option>';
+						}
+
+						$ret .= '
 					</select>
 					<input type="submit" name="' . self::$timepick . '" value="Change"/>
 			</form>
