9cee657c2757ec0e9471fe40dc9ad2d68434af8b
Linda Ott Olander <lo222hd@student.lnu.se>
2017-10-24 12:01:18 +0200

    Refactoring, old code in comments

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -70,6 +70,11 @@ class LoginController {
     $hasLoggedOut = $this->loginView->hasLoggedOut();
     $userNameMissing = $this->loginModel->isUserNameMissing();
     $passwordMissing = $this->loginModel->isPasswordMissing();
+    $wantsToRegisterUser = $this->registerView->wantsToRegisterUser();
+    $registerFormHasBeenPosted = $this->registerView->registerFormHasBeenPosted();
+    $registerUserNameOk = $this->registerModel->getIsRegisterNameOk();
+    $registerPasswordOk = $this->registerModel->getRegisterPasswordOk();
+    $showLoginViewResponse = true;
 
     //$userNameMissing = $this->loginView->checkUserNameMissing();
 
@@ -102,20 +107,23 @@ class LoginController {
         if ($cookieContentIsOk) {
           error_log("Cookie content ok", 3, "errors.log");
           $message = self::$welcomeBackMessage;
+          $this->isLoggedIn = true;
           $generateLogout = true;
         }
         //The cookie has wrong username and/or password, remove cookies
         else {
           error_log("Cookie content not ok", 3, "errors.log");
-          $this->view->removeCookies();
+          $message = "Wrong information in cookies";
+          $this->loginModel->removeCookies();
           $this->failedLoginAttempt = true;
         }
       }
       //The user is not logged in with session,
       //Is not logged in with cookies,
-      //But has just logged in successfully
+      //But has just tried to log in
       else if ($hasJustTriedToLogIn) {
         error_log("Has just tried to log in", 3, "errors.log");
+        //The log in was successful
         if ($isLoggedInWithForm) {
             error_log("Is logged in with form", 3, "errors.log");
             $this->isLoggedIn = true;
@@ -136,7 +144,7 @@ class LoginController {
               $message = 'Welcome and you will be remembered';
             }
       }
-      //Has tried to log in, but unsuccessfully
+      //Has just tried to log in, but is not logged in with form (log in was unsuccessful)
       else {
         error_log("Unsuccessful login", 3, "errors.log");
         //Check if username is missing
@@ -153,10 +161,47 @@ class LoginController {
           error_log("Wrong name or password", 3, "errors.log");
           $message = 'Wrong name or password';
         }
+      }
     }
-  }
+    //The user wants to register a new user, and has clicked the register link
+    else if ($wantsToRegisterUser) {
+      error_log("Wants to register user", 3, "errors.log");
+      if ($registerFormHasBeenPosted) {
+        error_log("Register form has been posted", 3, "errors.log");
+        if ($registerUserNameOk && $registerPasswordOk) {
+          error_log("Register username is ok and register password is ok", 3, "errors.log");
+          $message = "Registered new user";
+        }
+        else if ($registerUserNameOk == false && $registerPasswordOk == false) {
+          $message = "Username has too few characters, at least 3 characters. Password has too few characters, at least 6 characters.";
+          $showLoginViewResponse = false;
+        }
+        else {
+          $showLoginViewResponse = false;
+          if ($registerUserNameOk == false) {
+            $message = "Username has too few characters, at least 3 characters.";
+          }
+          else if ($registerPasswordOk == false) {
+            $message .= "Password has too few characters, at least 6 characters.";  
+          }
+        }
+      }
+      else {
+        $this->wantsToRegisterUser = true;
+        //Since the user now wants to register a new user, do not show the login view
+        $showLoginViewResponse = false;
+      }
+    }
 }
-    $this->response = $this->loginView->response($message,$generateLogout);
+
+    //Check if we should run the loginView response
+    if ($showLoginViewResponse) {
+        $this->response = $this->loginView->response($message,$generateLogout);
+    }
+    //If not, show the register view
+    else {
+      $this->response = $this->registerView->generateRegisterNewUserHTML($message);
+    }
 
     //Send information from what user wants to do to layout view and show the right view
     $this->layoutView->render($this->isLoggedIn, $this->failedLoginAttempt, $this->wantsToRegisterUser, $this->response);
